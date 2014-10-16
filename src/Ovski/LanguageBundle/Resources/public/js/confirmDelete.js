4d2ae860a0b2b1acdddc0c25657433b254240bf3
Ovski <baptiste.bouchereau@gmail.com>
2014-10-17 01:57:08 +0200

    Confirm delete on new translations made via ajax

--- src/Ovski/LanguageBundle/Resources/public/js/confirmDelete.js
+++ src/Ovski/LanguageBundle/Resources/public/js/confirmDelete.js
@@ -1,4 +1,4 @@
-jQuery('button.confirm-delete').on('click', function() {
+jQuery(document).on('click', 'button.confirm-delete', function() {
     if(confirm("Are you sure you want to delete this translation")) {
         return true;
     }
